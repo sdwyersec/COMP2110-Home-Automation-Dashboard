fac9cb14a55a38f9fa5d78711b231f3aa69d9259
yugsolanki2084 <160678138+yugsolanki2084@users.noreply.github.com>
2025-04-28 14:17:23 +1000

    corrected code

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 
 
 // base url of the backend server used for login, blog and advertising
-export const BASE_URL = 'http://localhost:8102/'; // 'https://comp2110-portal-server.fly.dev/';
+export const BASE_URL = 'https://comp2110-portal-server.fly.dev';
 
