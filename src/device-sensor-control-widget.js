f6a982a8c9a0b916667781fb656e0dc5bccfb391
yugsolanki2084 <160678138+yugsolanki2084@users.noreply.github.com>
2025-04-28 14:01:20 +1000

    done

--- src/device-sensor-control-widget.js
+++ src/device-sensor-control-widget.js
@@ -1,4 +1,5 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+
 
 export class DeviceSensorControlWidget extends LitElement {
   static styles = css`
